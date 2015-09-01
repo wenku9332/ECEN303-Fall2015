dd7a1e2f97dc2c05f4f273b66bbe35f1655d8a15
Jean-Francois Chamberland <chmbrlnd@users.noreply.github.com>
2015-09-01 13:20:41 -0500

    Update 1challenge.py

--- Miscellaneous/Templates/1challenge.py
+++ Miscellaneous/Templates/1challenge.py
@@ -15,27 +15,60 @@ Maximum Grade: 5pt
 
 import random
 import math
+import matplotlib.pyplot as plt
 
+
+ParameterP = 0.3
+NumberFlips = 8
 NumberTrials = 100000
 Trials = []
 
-def biasedcoinflip():
+
+def biasedcoinflip(p=0.5):
+    # EDIT
     # Create method for biased coin flip
+    #
 
-for TrialIndex in range(0, NumberTrials):
-    Trials.append(biasedcoinflip())
 
-TrialAverage = sum(Trials)/(1.0*len(Trials))
+for TrialIndex1 in range(0, NumberTrials):
+    Trials.append(biasedcoinflip(ParameterP))
+
+TrialAverage = sum(Trials) / (1.0 * len(Trials))
 print 'The average number of ones is {0:.4f}.'.format(TrialAverage)
 
 SumTrials = []
 
-for TrialIndex in range(0, NumberTrials):
-    # Add five coin flips for each SumTrials outcome
+for TrialIndex2 in range(0, NumberTrials):
+    # EDIT
+    # Add NumberFlips coin flips for each SumTrials outcome
+    #
 
 Distribution = []
-for OutcomeIndex in range(0,6):
-    Distribution.append(SumTrials.count(OutcomeIndex))
+for OutcomeIndex1 in range(0, NumberFlips + 1):
+    Distribution.append(SumTrials.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
 print repr(Distribution)
+# EDIT
+# Print the sum of the elements in Distribution
+#
 
+OutcomeIndex2 = range(0, NumberFlips + 1)
+num_bins = len(OutcomeIndex2)
+bar_width = 0.8
+XticksIndex = [(outcome + (0.5 * bar_width)) for outcome in OutcomeIndex2]
+opacity = 0.4
+
+plt.bar(OutcomeIndex2, Distribution, bar_width, alpha=opacity, color='b')
+plt.xlabel("Value")
+plt.ylabel("Probability")
+plt.xticks(XticksIndex, OutcomeIndex2)
+plt.show()
+
+"""
+Describe what happens to the figure as you vary ParameterP from zero to one.
+
+
+What is the most likely outcome for ParameterP = 0.7 and NumberFlips = 8?
+
+
+"""
