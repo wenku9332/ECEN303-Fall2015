965acf3825d3efac8463927d04739cec11299cc3
Fletcher Watts <augustus1994@tamu.edu>
2015-09-01 22:52:21 -0500

    Update Task1.py

--- Students/augustus1994/Task1.py
+++ Students/augustus1994/Task1.py
@@ -1,13 +1,10 @@
+
 import random
 
-Cardinality = 2
-NumberTrials = 100
-
-TrialSequence = []
-for TrialIndex in range(0, NumberTrials):
-    TrialSequence.append(random.randrange(Cardinality))
-
-EmpiricalDistribution = []
-for OutcomeIndex in range(0, Cardinality):
-    EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
-print EmpiricalDistribution
+def bernoulli(p):
+    
+    x = random.random()
+    
+    if x < p: return 1
+    
+    else: return 0
